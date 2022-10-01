7c3b387060d5e20bf7225ce13ff372a77efe1e70
Mohamed ibrahem <63398174+moibrahem98@users.noreply.github.com>
2022-10-02 00:59:22 +0300

    Time: 120 ms (97.95%), Space: 47.6 MB (47.76%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -1,24 +1,29 @@
 var romanToInt = function(s) {
-  const legend = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-  };
-  let total = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    console.log(legend[s[i]],legend[s[i]])
-    if (legend[s[i]] < legend[s[i + 1]]) {
-      total += legend[s[i + 1]] - legend[s[i]];
-      i++;
-    } else {
-      total += legend[s[i]];
+    
+    //Declare all combinations
+    const romans = {
+      combo: {IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900},
+      triple: {III: 3, XXX: 30, CCC: 300, MMM: 3000},
+      double: {II: 2, XX: 20, CC: 200, MM: 2000},
+      single: {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000},
     }
-  }
-
-  return total;
+    
+    let result = 0;
+    
+    //Go through every combination, first combo and last single
+    for (const type in romans) {
+      //Go through every key
+      for (const key in romans[type]) {
+        //Check if the key is included in the string
+        if (s.includes(key)) {
+          //Add the value to the total sum
+          result += romans[type][key];
+          //Remove the key from the string so it isn't summed again
+          s = s.replace(key, '');
+        }
+      }
+    }
+    
+    return result;
+    
 };
