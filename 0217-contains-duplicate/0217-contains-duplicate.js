41795afb2b6ea41f61d9432de0af5550b0adf188
Mohamed ibrahem <63398174+moibrahem98@users.noreply.github.com>
2022-11-05 13:02:11 +0300

    Time: 154 ms (45.70%), Space: 51 MB (47.74%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const numsSet = new Set(nums);/* Time O(N) | Space O(N) */
+    const isEqual = numsSet.size === nums.length;
+
+    return !isEqual;
+    
+};
