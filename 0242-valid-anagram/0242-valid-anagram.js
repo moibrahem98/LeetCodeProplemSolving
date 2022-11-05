9555c12e4910ce653dd874f81a53dbf57823f50a
Mohamed ibrahem <63398174+moibrahem98@users.noreply.github.com>
2022-11-05 13:20:33 +0300

    Time: 268 ms (5.68%), Space: 51.4 MB (5.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,7 @@
+var isAnagram = function (s, t) {
+    const isEqual = s.length === t.length
+    if (!isEqual) return false;
+    return reorder(s) === reorder(t)
+};
+
+const reorder = str => str.split("").sort((a, b) => a.localeCompare(b)).join("")
