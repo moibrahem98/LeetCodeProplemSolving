cc670b29f3526320163d952ea8bea3860a3f9500
Mohamed ibrahem <63398174+moibrahem98@users.noreply.github.com>
2022-11-06 10:08:56 +0300

    Time: 111 ms (80.28%), Space: 51.7 MB (51.65%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+        let [left, right, max] = [0, 1, 0];
+
+    while (right < prices.length) {
+        const canSlide = prices[right] <= prices[left];
+        if (canSlide) left = right;
+
+        const window = prices[right] - prices[left];
+
+        max = Math.max(max, window);
+        right++;
+    }
+
+    return max;
+};
